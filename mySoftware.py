7b24f76b6350e739fc98ae7d529062ff39133b57
reascr <94176469+reascr@users.noreply.github.com>
2024-05-03 14:22:20 +0200

    small changes

--- mySoftware.py
+++ mySoftware.py
@@ -1,24 +1,28 @@
-!pip install import_notebook
+#pip install import_notebook
 import argparse
 import os
 import json
 import pandas as pd
-from import_notebook import Notebook
+#from import_notebook import Notebook
 
-notebook = Notebook.load(os.path('data_pipeline.ipynb'))
-create_model_input = notebook.create_input_data # speicher es so ab wie beim Trainieren des Modells
+#notebook = Notebook.load(os.path('data_pipeline.ipynb'))
+#create_model_input = notebook.create_input_data # speicher es so ab wie beim Trainieren des Modells
 #from mistralus import model, predict # Model soll von Huggingface geladen werden, es werden Funktionen bereitgestellt
-# für jede Inputfile soll das dann ausgegeben werden
-# kann ich die data pipeline irgendwie importieren um unnötige Schreibarbeit zu umgehen
 
 def generate_output(input_dir, output_dir):
     #model = load_model() # load model from Huggingface
+    columns = ['paragraph1', 'paragraph2']
     for file_name in os.listdir(input_dir):
         counter = 1 # geht besser!
         if file_name.endswith(".txt"):
+            data = []
             with open(os.path.join(input_dir, file_name), "r") as f:
-                paragraphs = f.read()
-                # was für einen Input muss ich dem Model geben?
+                paragraphs = [str.strip(line) for line in f.readlines()]
+                for j in range(len(paragraphs)-1):
+                    para1 = paragraphs[j]
+                    para2 = paragraphs[j+1]
+                    data.append([para1,para2])
+                df = pd.DatFrame(data, columns=columns) # was für einen Input muss ich dem Model geben?
                 prediction = [0,1,1,1]# get predictions from model, should be a list, otherwise transform to list
                 output_file_name = f"solution-problem-{counter}.json"
                 counter += 1
